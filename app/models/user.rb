1d853443b998bfb355cb1154c989c8f549118c4a
shota-nagato <nagato@hab-co.jp>
2024-03-10 11:37:22 +0900

    ,忘れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name, length: { maximum: 50 }
-    validates :username, length: { maximum: 50 } uniqueness: true
+    validates :username, length: { maximum: 50 }, uniqueness: true
     validates :birth_date
     validates :email, length: { maximum: 255 }, uniqueness: true,
                       format: { with: VALID_EMAIL_REGEX }
