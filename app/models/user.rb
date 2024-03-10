2e9b10d73b85d561d5246e2703c7f898f361fda7
shota-nagato <nagato@hab-co.jp>
2024-03-10 10:57:05 +0900

    Userモデルのバリデーション

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  with_options presence: true do
+    validates :name, length: { maximum: 50 }
+    validates :username, length: { maximum: 50 } uniqueness: true
+    validates :birth_date
+    validates :email, length: { maximum: 255 }, uniqueness: true,
+                      format: { with: VALID_EMAIL_REGEX }
+  end
+
+  before_save { email.downcase! }
+end
