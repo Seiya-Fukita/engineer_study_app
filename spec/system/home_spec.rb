c28c8fa8fad69a656718d523e5cd3eb06004add3
shota-nagato <nagato@hab-co.jp>
2024-03-16 18:13:59 +0900

    standardrb --fix

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Home', type: :system, js: true do
-  it 'Homeという文字が存在する' do
+RSpec.describe "Home", type: :system, js: true do
+  it "Homeという文字が存在する" do
     visit root_path
 
-    expect(page).to have_content('Home')
+    expect(page).to have_content("Home")
   end
 end
