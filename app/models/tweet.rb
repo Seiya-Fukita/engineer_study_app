e5d835996a41e4af5918c7a6d0c71832064ae0e1
Seiya-Fukita <fukita@hab-co.jp>
2024-03-16 18:51:15 +0900

    gem image_processing追加＆userモデルとtweetモデル紐付け

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,6 @@
 class tweet < ApplicationRecord
+  belongs_to :user
+  has_many_attached :images
 
   validates :content, presence: true, length: {maximum: 140}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_URL_REGEX = /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
 
