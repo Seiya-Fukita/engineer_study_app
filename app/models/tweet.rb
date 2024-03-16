5821767021c4e06be6c53f3c0447a2ca42b6ab00
Seiya-Fukita <fukita@hab-co.jp>
2024-03-16 18:56:15 +0900

    CI修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
-class tweet < ApplicationRecord
+class Tweet < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validates :content, presence: true, length: {maximum: 140}
+  validate :content, presence: true, length: {maximum: 140}
 end
