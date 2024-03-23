3c1b07583cef41facd78fab33b426d2e5645349f
Seiya-Fukita <fukita@hab-co.jp>
2024-03-23 15:15:12 +0900

    Rspec作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many_attached :images
 
-  validate :content, presence: true, length: {maximum: 140}
+  validates :content, presence: true, length: {maximum: 140}
 end
---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe Tweet, type: :model do
+  describe "バリデーション" do
+    let(:tweet) { build(:tweet, content: content) }
+
+    context "内容が140字以内の場合" do
+      let(:content) {"TEST"}
+
+      it "正常に作成できる" do
+        expect(tweet).to be_valid
+      end
+    end
+
+    context "内容が140字より大きい場合" do
+      let(:content) { "a" * 141 }
+      it "エラーメッセージが返る" do
+        expect(tweet).to be_invalid
+        expect(tweet.errors.full_messages).to eq ["内容は140文字以内で入力してください"]
+      end
+    end
+
+    context "内容が空の場合" do
+      let(:content) {""}
+      it "エラーメッセージが返る" do
+        expect(tweet).to be_invalid
+        expect(tweet.errors.full_messages).to eq ["内容を入力してください"]
+      end
+    end
+  end
+end
