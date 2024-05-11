51d1a82894565db2374ffb7bd30a0ec8b80a4c30
Seiya-Fukita <fukita@hab-co.jp>
2024-05-11 17:33:16 +0900

    standard_fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  root 'home#index'
+  root "home#index"
 
-  get 'top' => 'home#top'
-  get 'sign_up' => 'users/registrations#new'
-  get 'password' => 'users/passwords#new'
-  get 'confirmation' => 'users/confirmations#new'
+  get "top" => "home#top"
+  get "sign_up" => "users/registrations#new"
+  get "password" => "users/passwords#new"
+  get "confirmation" => "users/confirmations#new"
   namespace :users do
     resources :confirmations
     resources :registrations
